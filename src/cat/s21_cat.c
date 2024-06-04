7532f2e76090bce21375264cd5196541306f88c9
Jutta Christmas <juttachr@mi-i1.msk.21-school.ru>
2024-06-04 19:28:01 +0300

    cat

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
       fileHandlerCat(stream, arguments);
     }
     i++;
-    fclose(stream);
+    if (stream != NULL) fclose(stream);
   } while (i < argc);
 }
 
